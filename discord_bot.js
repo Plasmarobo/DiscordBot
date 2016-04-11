b1765210671138a31559abd4cae0e90f35ad47f9
Plasmarobo <plasmarobo@gmail.com>
2016-04-11 19:12:31 -0400

    Fixed duplicate

--- discord_bot.js
+++ discord_bot.js
@@ -375,7 +375,7 @@ bot.on("message", function (msg) {
     }
   }
 
-  var who = msg.content.match(/\<\@([0-9]+)\>\s\<\@([0-9]+)\>\swho\sis\s\<\@([0-9]+)\>/i)
+  var who = msg.content.match(/\<\@([0-9]+)\>\swho\sis\s\<\@([0-9]+)\>/i)
   if (who != null)
   {
     console.log(who);
